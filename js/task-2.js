65fc39261576dbdf8ad76e157e4d61cdab88d6cc
ValeryMaz <mazhytova1996@gmail.com>
2025-02-15 21:42:57 -0500

    tasks done

---
+++ js/task-2.js
@@ -0,0 +1,47 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  },
+];
+
+// Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+
+// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+const gallery = document.querySelector(".gallery");
+// console.log("🚀 ~ gallery:", gallery);
+const galleryImages = images
+  .map(({ url, alt }) => {
+    return `<li class="gallery-item"><img src="${url}" alt="${alt}" width="250"/></li>`;
+  })
+  .join("");
+// console.log("🚀 ~ galleryImages ~ galleryImages:", galleryImages);
+gallery.insertAdjacentHTML("afterbegin", galleryImages);
---
+++ js/task-5.js
@@ -0,0 +1,53 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Фон на <body> задається тільки після кліку на button.change-color
+// При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
+// На <body> і span.color значення одного й того самого кольору
+
+const button = document.querySelector(".change-color");
+const newSpan = document.querySelector(".color");
+
+// button.style.backgroundColor = function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// };
+
+button.addEventListener("click", changeBgColor);
+
+function changeBgColor() {
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+  newSpan.textContent = newColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+console.log("🚀 ~ button:", button);
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
